a60ffefc2a3df50497f1f6ba1eeaaf4a6c67af5d
Siddhesh Desai <siddheshdesai777@gmail.com>
2025-01-17 01:39:04 +0530

    Added path

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.urlencoded());
 
 // view engine
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use("/", generalRoutes);
